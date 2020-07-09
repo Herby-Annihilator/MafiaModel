f931ddb2f5a76746431bbc08c698bc00e44d420c
Danil Rukin <danil2018d@yandex.ru>
2020-07-09 16:27:17 +0700

    Начал делать UI

---
+++ UI/MafiaModelUI.java
@@ -0,0 +1,87 @@
+package UI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MafiaModelUI {
+
+    @FXML
+    private Label firstPlayerName;
+
+    @FXML
+    private Label firstPlayerRole;
+
+    @FXML
+    private Label secondPlayerName;
+
+    @FXML
+    private Label secondPlayerRole;
+
+    @FXML
+    private Label thirdPlayerName;
+
+    @FXML
+    private Label thirdPlayerRole;
+
+    @FXML
+    private Label fourthPlayerName;
+
+    @FXML
+    private Label fourthPlayerRole;
+
+    @FXML
+    private Label fifthPlayerName;
+
+    @FXML
+    private Label fifthPlayerRole;
+
+    @FXML
+    private Label sixthPlayerName;
+
+    @FXML
+    private Label sixthPlayerRole;
+
+    @FXML
+    private Label seventhPlayerName;
+
+    @FXML
+    private Label seventhPlayerRole;
+
+    @FXML
+    private Label eighthPlayerName;
+
+    @FXML
+    private Label eighthPlayerRole;
+
+    @FXML
+    private Label ninthPlayerName;
+
+    @FXML
+    private Label ninthPlayerRole;
+
+    @FXML
+    private Label tenthPlayerName;
+
+    @FXML
+    private Label tenthPlayerRole;
+
+    @FXML
+    private Label eleventhPlayerName;
+
+    @FXML
+    private Label eleventhPlayerRole;
+
+    @FXML
+    private Label twelfthPlayerName;
+
+    @FXML
+    private Label twelfthPlayerRole;
+
+    @FXML
+    private Button startGameBtn;
+
+    @FXML
+    private Button endGameBtn;
+
+}
